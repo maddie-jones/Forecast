b874ade6ba43b65000ec225936978d8d1ce0db4b
Maddie Jones <maddyg91@gmail.com>
2019-03-15 18:31:57 -0600

    pushing to github pages

--- main.bundle.js
+++ main.bundle.js
@@ -46,5 +46,113 @@
 
 	"use strict";
 
+	var location = document.getElementById("location");
+	var locationButton = document.getElementById("location-button");
+	var favoriteButton = document.getElementById("favorite-button");
+
+	function background(location) {
+	  fetch("https://sweater-weather-mine.herokuapp.com/api/v1/backgrounds?location=" + location).then(function (response) {
+	    return response.json();
+	  }).then(function (data) {
+	    return renderBackground(data);
+	  }).catch(function (error) {
+	    return console.error({ error: error });
+	  });
+	}
+
+	function renderBackground(data) {
+	  var url = data["data"]["attributes"].backgroud_url;
+	  document.getElementById('background').style.backgroundImage = "url(" + url + ")";
+	}
+
+	function weatherAtLocation() {
+	  var userLocation = location.value;
+	  if (userLocation) {
+	    background(userLocation);
+	    forecast(userLocation);
+	    document.getElementById("change-button").style.visibility = "visible";
+	    document.getElementById("change-button").style.visibility = "visible";
+	    document.getElementById("favorite-button").style.visibility = "visible";
+	    document.getElementById("details").innerHTML = "Details";
+	    document.getElementById("city").innerHTML = userLocation;
+	  };
+	}
+
+	function renderForecast(data) {
+	  var summary = data["data"]["attributes"]["currently"].summary;
+	  var temp = data["data"]["attributes"]["currently"].temperature;
+	  document.getElementById("summary").innerHTML = summary;
+	  document.getElementById("temp").innerHTML = temp + "°";
+	}
+
+	function forecast(location) {
+	  fetch("https://sweater-weather-mine.herokuapp.com/api/v1/forecast?location=" + location).then(function (response) {
+	    return response.json();
+	  }).then(function (data) {
+	    return renderForecast(data);
+	  }).catch(function (error) {
+	    return console.error({ error: error });
+	  });
+	}
+
+	function renderFavorites(data) {
+	  var attributes = data["data"];
+	  var _iteratorNormalCompletion = true;
+	  var _didIteratorError = false;
+	  var _iteratorError = undefined;
+
+	  try {
+	    for (var _iterator = attributes[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+	      var value = _step.value;
+
+	      var pel = document.createElement("P");
+	      var t = document.createTextNode(value["attributes"]["location"]);
+	      pel.appendChild(t);
+	      document.getElementById("current-favorites").appendChild(pel);
+	    }
+	  } catch (err) {
+	    _didIteratorError = true;
+	    _iteratorError = err;
+	  } finally {
+	    try {
+	      if (!_iteratorNormalCompletion && _iterator.return) {
+	        _iterator.return();
+	      }
+	    } finally {
+	      if (_didIteratorError) {
+	        throw _iteratorError;
+	      }
+	    }
+	  }
+	}
+
+	function listFavorites(location) {
+	  fetch("https://sweater-weather-mine.herokuapp.com/api/v1/favorites?api_key=7ca3f5426c369b8763a73fc0c4").then(function (response) {
+	    return response.json();
+	  }).then(function (data) {
+	    return renderFavorites(data);
+	  }).catch(function (error) {
+	    return console.error({ error: error });
+	  });
+	}
+
+	function favorite(location) {
+	  fetch("https://sweater-weather-mine.herokuapp.com/api/v1/favorites?location=" + location + "&api_key=7ca3f5426c369b8763a73fc0c4", {
+	    method: 'post',
+	    headers: {
+	      'Content-Type': 'application/json'
+	    }
+	  });
+	}
+
+	function addFavorite() {
+	  var userLocation = location.value;
+	  favorite(userLocation);
+	  listFavorites(userLocation);
+	}
+
+	locationButton.addEventListener('click', weatherAtLocation);
+	favoriteButton.addEventListener('click', addFavorite);
+
 /***/ })
 /******/ ]);
